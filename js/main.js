6de77169133cf1213d8445b3de766e5f0c7fc7af
mainzonder <48353743+mainzonder@users.noreply.github.com>
2020-06-01 10:48:58 +0300

    five

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
-$(function(){
+$(function () {
+	$(".top-slider").slick({
+		dots: true,
 
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		responsive: [
+			{
+				breakpoint: 1024,
+				settings: {},
+			},
+			{
+				breakpoint: 640,
+				settings: {
+					arrows: false,
+				},
+			},
+		],
+	});
 });
